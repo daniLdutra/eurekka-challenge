0ddb49d42969b7ec7fc7aa00ba5f35132a1ab0b1
Daniella Dutra <dani.ldutra@gmail.com>
2022-07-04 14:20:28 -0300

    feat: validation for post route

--- back-eurekka/src/routes/reportRoutes.js
+++ back-eurekka/src/routes/reportRoutes.js
@@ -1,11 +1,29 @@
 const { Router } = require('express');
 const report = require('../model/Report');
+const { body, validationResult } = require('express-validator');
 
 const reportRoutes = Router();
 
-reportRoutes.post('/', async (req, res) => {
+const createReportValidation = [
+  body('veterinary').isLength({ min: 2, max: 40 }),
+  body('guardian').isLength({ min: 2, max: 40 }),
+  body('pacient').isLength({ min: 2, max: 40 }),
+  body('specie').isLength({ min: 2, max: 10 }),
+  body('breed').isLength({ min: 2, max: 20 }),
+  body('sex').isLength({ min: 1, max: 1 }),
+  body('age').isNumeric({ no_symbols: true }),
+  body('region').isLength({ min: 2, max: 20 }),
+  body('report').isLength({ min: 5, max: 255 }),
+];
+
+reportRoutes.post('/', createReportValidation, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   const created = await report.create(req.body);
-  return res.status(200).json(created);
+  return res.status(201).json(created);
 });
 
 module.exports = { reportRoutes };
