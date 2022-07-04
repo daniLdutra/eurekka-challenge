be0f19c6442bc34db8424f9941521aeee0327f35
Daniella Dutra <dani.ldutra@gmail.com>
2022-07-04 11:24:59 -0300

    feat: report model created

---
+++ back-eurekka/src/model/Report.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const ReportSchema = new mongoose.Schema(
+  {
+    veterinary: {
+      type: String,
+      required: true,
+    },
+    guardian: {
+      type: String,
+      required: true,
+    },
+    pacient: {
+      type: String,
+      required: true,
+    },
+    specie: {
+      type: String,
+      required: true,
+    },
+    breed: {
+      type: String,
+    },
+    sex: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    region: {
+      type: String,
+      required: true,
+    },
+    report: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const report = mongoose.model('report', ReportSchema);
+
+module.exports = report;
