47543f531449c1ae9aa6084d20076882579c60e4
Daniella Dutra <dani.ldutra@gmail.com>
2022-07-04 17:53:20 -0300

    feat: implementing cors on API routes

--- back-eurekka/src/server.js
+++ back-eurekka/src/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { db } = require('./config/db');
 const { reportRoutes } = require('./routes/reportRoutes');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 const PORT = process.env.PORT || 3001;
 
