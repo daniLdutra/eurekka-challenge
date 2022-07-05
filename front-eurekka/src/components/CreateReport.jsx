feec51ad78aca3507568da58822b42e61518f3de
Daniella Dutra <dani.ldutra@gmail.com>
2022-07-05 12:28:42 -0300

    feat: form state done

--- front-eurekka/src/components/CreateReport.jsx
+++ front-eurekka/src/components/CreateReport.jsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
 
 export const CreateReport = () => {
+  const [values, setValues] = useState({});
+
+  const handleChange = (e) => {
+    const newValues = values;
+
+    newValues[e.target.name] = e.target.value;
+    setValues(newValues);
+  };
+
   return (
     <div>
       <Container className="justify-content-md-center">
@@ -8,32 +18,58 @@ export const CreateReport = () => {
           <h2>Criar Laudo</h2>
           <Form.Group className="mb-3">
             <Form.Label>Veterinário solicitante</Form.Label>
-            <Form.Control type="veterinary" />
+            <Form.Control
+              value={values.veterinary}
+              name="veterinary"
+              onChange={handleChange}
+              type="veterinary"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Tutor</Form.Label>
-            <Form.Control type="guardian" />
+            <Form.Control
+              value={values.guardian}
+              name="guardian"
+              onChange={handleChange}
+              type="guardian"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Paciente</Form.Label>
-            <Form.Control type="pacient" />
+            <Form.Control
+              value={values.pacient}
+              name="pacient"
+              onChange={handleChange}
+              type="pacient"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Espécie</Form.Label>
-            <Form.Control type="specie" />
+            <Form.Control
+              value={values.specie}
+              name="specie"
+              onChange={handleChange}
+              type="specie"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Raça</Form.Label>
-            <Form.Control type="breed" />
+            <Form.Control
+              value={values.breed}
+              name="breed"
+              onChange={handleChange}
+              type="breed"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Sexo</Form.Label>
-            <Form.Select>
+            <Form.Select value={values.sex} name="sex" onChange={handleChange}>
+              <option value="">Selecione uma opção</option>
               <option value="F">Fêmea</option>
               <option value="M">Macho</option>
             </Form.Select>
@@ -41,12 +77,22 @@ export const CreateReport = () => {
 
           <Form.Group className="mb-3">
             <Form.Label>Idade</Form.Label>
-            <Form.Control type="age" />
+            <Form.Control
+              value={values.age}
+              name="age"
+              onChange={handleChange}
+              type="age"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Região</Form.Label>
-            <Form.Control type="region" />
+            <Form.Control
+              value={values.region}
+              name="region"
+              onChange={handleChange}
+              type="region"
+            />
           </Form.Group>
 
           <div>
@@ -57,6 +103,9 @@ export const CreateReport = () => {
               className="form-control"
               id="exampleFormControlTextarea1"
               rows={5}
+              value={values.report}
+              name="report"
+              onChange={handleChange}
             />
           </div>
           <Button variant="primary" type="submit">
